a9d93b3e7c6a7bb3fd0c0ad115ae4087bd032d10
Rezvyh <rezvyhlilija@gmail.com>
2026-08-22 11:14:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-def main():
-    print("Hello from repl-nix-workspace!")
-
+from astro.main import app, bot, scheduler
+import threading
 
 if __name__ == "__main__":
-    main()
+    # Запускаем Flask-приложение в отдельном потоке
+    threading.Thread(
+        target=lambda: app.run(host="0.0.0.0", port=8000), daemon=True
+    ).start()
+
+    # Запускаем бота и планировщик
+    bot.polling()
